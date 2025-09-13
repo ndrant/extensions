2c983bf938573377aa9d48beeca37b55dffad318
Kaushal Bhingaradia <75636044+kashrtx@users.noreply.github.com>
2025-09-13 17:33:34 -0400

    Update AnimePahe extension to version 0.0.81 -- Updated to latest .si domain
    - AnimePahe switched domains from .ru to .si.  Applied new fix for extension to work again

--- dart/anime/src/en/animepahe/source.dart
+++ dart/anime/src/en/animepahe/source.dart
@@ -1,12 +1,13 @@
 import '../../../../../model/source.dart';
 
 Source get animepaheSource => _animepaheSource;
-const _animepaheVersion = "0.0.75";
+const _animepaheVersion = "0.0.81";
 const _animepaheSourceCodeUrl =
     "https://raw.githubusercontent.com/m2k3a/mangayomi-extensions/$branchName/dart/anime/src/en/animepahe/animepahe.dart";
 Source _animepaheSource = Source(
   name: "AnimePahe",
-  baseUrl: "https://www.animepahe.ru",
+  //baseUrl: "https://www.animepahe.ru",
+  baseUrl: "https://animepahe.si", // new domain as of September 10, 2025
   lang: "en",
   typeSource: "single",
   iconUrl:
